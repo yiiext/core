b33bcdc4e120aa790ce83a1e7ee496a1f81e426b
Veaceslav Medvedev <slavcopost@gmail.com>
2009-12-29 19:34:13 +0000

    Объеденил весь sql в один файл для удобства импорта в myPhpAdmin Добавил DROP TABLE во избежании конфликтов версий
    git-svn-id: http://yiiext.googlecode.com/svn/trunk@76 175f29da-e50b-11de-93f0-cd01e99e34ab

---
+++ app/tests/sql/all.sql
@@ -0,0 +1,59 @@
+/* Book table */
+DROP TABLE IF EXISTS `Book`;
+CREATE TABLE IF NOT EXISTS `Book` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `price` FLOAT NOT NULL,
+  PRIMARY KEY  (`id`)
+);              
+
+/* Fruit table */
+DROP TABLE IF EXISTS `Fruit`;
+CREATE TABLE IF NOT EXISTS `Fruit` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `deleted` BOOL NOT NULL DEFAULT 0,
+  PRIMARY KEY  (`id`)
+);              
+
+/* Post table */
+DROP TABLE IF EXISTS `Post`;
+CREATE TABLE IF NOT EXISTS `Post` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `status` VARCHAR(255) NOT NULL DEFAULT 'published',
+  PRIMARY KEY  (`id`)
+);
+
+/* Tag table */
+DROP TABLE IF EXISTS `Tag`;
+CREATE TABLE IF NOT EXISTS `Tag` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `Tag_name` (`name`)
+);
+
+/* Tag binding table */
+DROP TABLE IF EXISTS `PostTag`;
+CREATE TABLE IF NOT EXISTS `PostTag` (
+  `postId` INT(10) UNSIGNED NOT NULL,
+  `tagId` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY  (`postId`,`tagId`)
+);
+
+/* Tag table */
+DROP TABLE IF EXISTS `Color`;
+CREATE TABLE IF NOT EXISTS `Color` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `Color_name` (`name`)
+);
+
+/* Tag binding table */
+DROP TABLE IF EXISTS `PostColor`;
+CREATE TABLE IF NOT EXISTS `PostColor` (
+  `postId` INT(10) UNSIGNED NOT NULL,
+  `colorId` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY  (`postId`,`colorId`)
+);
