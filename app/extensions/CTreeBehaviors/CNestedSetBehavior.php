615e4fa930f13a66f40178076a14f353553ca47c
Alexander Kochetov <creocoder@gmail.com>
2010-02-16 05:04:50 +0000

    New named scopes based CNestedSetBehavior v0.2
    git-svn-id: http://yiiext.googlecode.com/svn/trunk@114 175f29da-e50b-11de-93f0-cd01e99e34ab

--- app/extensions/CTreeBehaviors/CNestedSetBehavior.php
+++ app/extensions/CTreeBehaviors/CNestedSetBehavior.php
@@ -1,63 +1,197 @@
 <?php
 /**
-* Основная мотивация написания, это то, что этот класс, в отличие от того,
-* что в extensions позволит вытаскивать категории вместе со связанными моделями.
+* NestedSetBehavior
 *
-* Например:
-* $criteria=new CDbCriteria(array(
-* 	'condition'=>'products.weight>?',
-* 	'params'=>array(40),
-* 	'with'=>array(
-* 		'products'=>array(
-* 			'joinType'=>'INNER JOIN',
-* 		),
-* 	)
-* ));
-* $category->findChilds($criteria);
-* вытащит только те категории продкуты в которых весят больше 40г
-*
-* Если делать через named space, то код выглядит ещё проще и CDbCriteria можно не создавать.
-*
-* Далее идут задания себе. Советы с аргументацией можно давать указывая в скобках ник.
-*
-* Например:
-* TODO: (creocoder)нужен ли при варианте findParents(findPath) реализованном в виде named space?
-* TODO: (someone)По идее нет, родителя можно вытащить так $category->pathOf()->find(). Но это для искушенных.
-*
-* Поехали:
-* TODO: (creocoder)проверить все(!) возможные исключения
-* TODO: (creocoder)написать тесты
-* TODO: (creocoder)реализовать актуализацию дерева в уже созданных во время выполнения объектах. Через Yii Events, либо паттер Registry?
-* TODO: (creocoder)наследоваться от CTreeBehavior или реализовывать интерфейс ITree? (преимущества, недостатки)
-*       (samdark)занятней всего будет выделить общий функционал в abstract CTreeBehaviour для последующей реализации других способов хранение дерева 
-* TODO: (creocoder)доработать с учетом 't' префикса
-* TODO: (creocoder)смущает постоянная проверка $this->hasManyRoots. Вынести этот функционал в дочерний CNestedSetWithManyRootsBehavior,
-* что ускорит работу методов, когда не нужны множественные корни?
-*       (samdark) На скорости сильно не скажется. Можно оставить как есть.
-* TODO: (creocoder)реализовать поддержку множественных корней находящихся в другой таблице? 
-* TODO: (creocoder)использовать ли quoteColumn() метод для обрамления всех полей в кавычки? (распухнут 'condition')
-*
-* TODO: (creocoder)-== в перспективе ==-
-* TODO: (creocoder)реализовать CAjListBehavior + CAjListWithManyRootsBehavior
-* TODO: (creocoder)реализовать возможность конвертирования одного дерева в другое
-* TODO: (creocoder)реализовать методы для тестирования целостности дерева
-* TODO: (creocoder)DAO? :-)
+* @version 0.2
+* @author creocoder <creocoder@gmail.com>
 */
 
 class CNestedSetBehavior extends CActiveRecordBehavior
 {
-    /**
-     * @var boolean Хранится ли в таблице более одного дерева
-     */
 	public $hasManyRoots=false;
-
 	public $root='root';
 	public $left='lft';
 	public $right='rgt';
 	public $level='level';
 
+	/**
+	* Named scope. Gets children for node (direct descendants only).
+	* @return CActiveRecord the owner component.
+	*/
+	public function children()
+	{
+		return $this->descendants(1);
+	}
+
+	/**
+	* Named scope. Gets descendants for node.
+	* @param int depth.
+	* @return CActiveRecord the owner component.
+	*/
+	public function descendants($depth=null)
+	{
+		$owner=$this->getOwner();
+		$criteria=$owner->getDbCriteria();
+		$alias=$criteria->alias===null ? 't' : $criteria->alias; //TODO: watch issue 914
+
+		$criteria->mergeWith(array(
+			'condition'=>$alias.'.'.$this->left.'>'.$owner->getAttribute($this->left).
+				' AND '.$alias.'.'.$this->right.'<'.$owner->getAttribute($this->right),
+			'order'=>$alias.'.'.$this->left,
+		));
+
+		if($depth!==null)
+			$criteria->addCondition($alias.'.'.$this->level.'<='.($owner->getAttribute($this->level)+$depth));
+
+		if($this->hasManyRoots)
+			$criteria->addCondition($alias.'.'.$this->root.'='.$owner->getAttribute($this->root));
+
+		return $owner;
+	}
+
+	/**
+	* Named scope. Gets ancestors for node.
+	* @param int depth.
+	* @return CActiveRecord the owner component.
+	*/
+	public function ancestors($depth=null)
+	{
+		$owner=$this->getOwner();
+		$criteria=$owner->getDbCriteria();
+		$alias=$criteria->alias===null ? 't' : $criteria->alias; //TODO: watch issue 914
+
+		$criteria->mergeWith(array(
+			'condition'=>$alias.'.'.$this->left.'<'.$owner->getAttribute($this->left).
+				' AND '.$alias.'.'.$this->right.'>'.$owner->getAttribute($this->right),
+			'order'=>$alias.'.'.$this->left,
+		));
+
+		if($depth!==null)
+			$criteria->addCondition($alias.'.'.$this->level.'>='.($owner->getAttribute($this->level)+$depth));
+
+		if($this->hasManyRoots)
+			$criteria->addCondition($alias.'.'.$this->root.'='.$owner->getAttribute($this->root));
+
+		return $owner;
+	}
+
+	/**
+	* Gets record of node parent.
+	* @return CActiveRecord the record found.
+	*/
+	public function parent()
+	{
+		$owner=$this->getOwner();
+		$criteria=$owner->getDbCriteria();
+		$alias=$criteria->alias===null ? 't' : $criteria->alias; //TODO: watch issue 914
+
+		$criteria->mergeWith(array(
+			'condition'=>$alias.'.'.$this->left.'<'.$owner->getAttribute($this->left).
+				' AND '.$alias.'.'.$this->right.'>'.$owner->getAttribute($this->right),
+			'order'=>$alias.'.'.$this->right,
+		));
+
+		if($this->hasManyRoots)
+			$criteria->addCondition($alias.'.'.$this->root.'='.$owner->getAttribute($this->root));
+
+		return $owner->find();
+	}
+
+	public function appendChild($node)
+	{
+		if(!$node->validate())
+			return false;
+
+		$owner=$this->getOwner();
+		$transaction=$owner->getDbConnection()->beginTransaction();
+
+		try
+		{
+			$condition=$this->left.'>'.$owner->getAttribute($this->right);
+
+			if($this->hasManyRoots)
+				$condition.=' AND '.$this->root.'='.$owner->getAttribute($this->root);
+
+			$owner->updateAll(array($this->left=>new CDbExpression($this->left.'+2')),$condition);
+			$condition=$this->right.'>='.$owner->getAttribute($this->right);
+
+			if($this->hasManyRoots)
+				$condition.=' AND '.$this->root.'='.$owner->getAttribute($this->root);
+
+			$owner->updateAll(array($this->right=>new CDbExpression($this->right.'+2')),$condition);
+			$node->setAttribute($this->left,$owner->getAttribute($this->right));
+			$node->setAttribute($this->right,$owner->getAttribute($this->right)+1);
+			$node->setAttribute($this->level,$owner->getAttribute($this->level)+1);
+
+			if($this->hasManyRoots)
+				$node->setAttribute($this->root,$owner->getAttribute($this->root));
+
+			$node->save(false);
+			$transaction->commit();
+
+			return true;
+		}
+		catch(Exception $e)
+		{
+			$transaction->rollBack();
+
+			return false;
+		}
+	}
+
+	public function deleteNode()
+	{
+		$owner=$this->getOwner();
+		$transaction=$owner->getDbConnection()->beginTransaction();
+
+		try
+		{
+			$condition=$this->left.'>='.$owner->getAttribute($this->left).' AND '.
+				$this->right.'<='.$owner->getAttribute($this->right);
+
+			if($this->hasManyRoots)
+				$condition.=' AND '.$this->root.'='.$owner->getAttribute($this->root);
+
+			$owner->deleteAll($condition);
+			$width=$owner->getAttribute($this->right)-$owner->getAttribute($this->left)+1;
+			$condition=$this->left.'>'.$owner->getAttribute($this->right);
+
+			if($this->hasManyRoots)
+				$condition.=' AND '.$this->root.'='.$owner->getAttribute($this->root);
+
+			$owner->updateAll(array($this->left=>new CDbExpression($this->left.'-'.$width)),$condition);
+			$condition=$this->right.'>'.$owner->getAttribute($this->right);
+
+			if($this->hasManyRoots)
+				$condition.=' AND '.$this->root.'='.$owner->getAttribute($this->root);
+
+			$owner->updateAll(array($this->right=>new CDbExpression($this->right.'-'.$width)),$condition);
+			$transaction->commit();
+
+			return true;
+		}
+		catch(Exception $e)
+		{
+			$transaction->rollBack();
+
+			return false;
+		}
+	}
+
+	public function isLeaf()
+	{
+		return $this->getOwner()->getAttribute($this->right)-$this->getOwner()->getAttribute($this->left)===1;
+	}
+
+	public function isRoot()
+	{
+		return !(boolean)$this->getOwner()->getAttribute($this->level);
+	}
+
+
+	/* Begin all deprecated */
+
 	//TODO: (creocoder)реализовать в виде named space и/или добавить $criteria, как аргумент
-    // (samdark) нет слова Childs, есть Children
 	public function findChilds()
 	{
 		$criteria=new CDbCriteria(array(
@@ -74,7 +208,6 @@ class CNestedSetBehavior extends CActiveRecordBehavior
 	}
 
 	//TODO: (creocoder)переименовать в findDescendants()?
-    // (samdark): да.
 	//TODO: (creocoder)реализовать в виде named space и/или добавить $criteria, как аргумент
 	public function findAllChilds()
 	{
@@ -91,7 +224,6 @@ class CNestedSetBehavior extends CActiveRecordBehavior
 	}
 
 	//TODO: (creocoder)переименовать в findPath()?
-    // (samdark) да.
 	//TODO: (creocoder)реализовать в виде named space и/или добавить $criteria, как аргумент
 	public function findParents()
 	{
@@ -113,110 +245,10 @@ class CNestedSetBehavior extends CActiveRecordBehavior
 		return;
 	}
 
-	public function appendChild($node)
-	{
-		if(!$node->validate())
-			return false;
-
-		$transaction=$this->owner->getDbConnection()->beginTransaction();
-
-		try
-		{
-			$condition=$this->left.'>'.$this->owner->getAttribute($this->right);
-
-			if($this->hasManyRoots)
-				$condition.=' AND '.$this->root.'='.$this->owner->getAttribute($this->root);
-
-			$this->owner->updateAll(array($this->left=>new CDbExpression($this->left.'+2')),$condition);
-
-			$condition=$this->right.'>='.$this->owner->getAttribute($this->right);
-
-			if($this->hasManyRoots)
-				$condition.=' AND '.$this->root.'='.$this->owner->getAttribute($this->root);
-
-			$this->owner->updateAll(array($this->right=>new CDbExpression($this->right.'+2')),$condition);
-
-			$node->{$this->left}=$this->owner->getAttribute($this->right);
-			$node->{$this->right}=$this->owner->getAttribute($this->right)+1;
-			$node->{$this->level}=$this->owner->getAttribute($this->level)+1;
-
-			if($this->hasManyRoots)
-				$node->{$this->root}=$this->owner->getAttribute($this->root);
-
-			$node->save(false);
-
-			$transaction->commit();
-
-			return true;
-		}
-		catch(Exception $e)
-		{
-			$transaction->rollBack();
-
-			return false;
-		}
-	}
 
 	//TODO: (creocoder)есть возможность сделать через beforeDelete() возвращающий в итоге false, тогда будет возможно
 	// просто $node->delete(), но повлияет на нижестоящие в behaviors() поведения, обсудить
-    // (samdark) можно просто перекрыть delete() без вызова parent::delete().
-    // Обязательно в этом случае триггернуть события onBeforeDelete и onAfterDelete.
 	//TODO: (creocoder)учесть возможность переноса дочерних категорий на уровень вверх, вместо их удаления
-    // (samdark) бесполезная фича. Если удаляют, вложенные тоже убивают.
-	public function deleteNode()
-	{
-		$transaction=$this->owner->getDbConnection()->beginTransaction();
-
-		try
-		{
-			$condition=$this->left.'>='.$this->owner->getAttribute($this->left).' AND '.
-				$this->right.'<='.$this->owner->getAttribute($this->right);
-
-			if($this->hasManyRoots)
-				$condition.=' AND '.$this->root.'='.$this->owner->getAttribute($this->root);
-
-			$this->owner->deleteAll($condition);
-
-			$width=$this->owner->getAttribute($this->right)-$this->owner->getAttribute($this->left)+1;
-
-			$condition=$this->left.'>'.$this->owner->getAttribute($this->right);
-
-			if($this->hasManyRoots)
-				$condition.=' AND '.$this->root.'='.$this->owner->getAttribute($this->root);
-
-			$this->owner->updateAll(array($this->left=>new CDbExpression($this->left.'-'.$width)),$condition);
-
-			$condition=$this->right.'>'.$this->owner->getAttribute($this->right);
-
-			if($this->hasManyRoots)
-				$condition.=' AND '.$this->root.'='.$this->owner->getAttribute($this->root);
-
-			$this->owner->updateAll(array($this->right=>new CDbExpression($this->right.'-'.$width)),$condition);
-
-			$transaction->commit();
-
-			return true;
-		}
-		catch(Exception $e)
-		{
-			$transaction->rollBack();
-
-			return false;
-		}
-	}
-
-	public function isLeaf()
-	{
-		return $this->owner->getAttribute($this->right)-$this->owner->getAttribute($this->left)==1;
-	}
-
-	//TODO: (creocoder)может лучше return (bool)$this->owner->getAttribute($this->level)?
-    // (samdark) да.
-	public function isRoot()
-	{
-		return $this->owner->getAttribute($this->level)==0;
-	}
-
 	//TODO: (creocoder)уточнить про уровень
 	public function getPrevSibling()
 	{
@@ -250,4 +282,6 @@ class CNestedSetBehavior extends CActiveRecordBehavior
 	{
 		//TODO: (creocoder)Не забыть выкидывать CException при $this->hasManyRoots==false. Категория для Yii::t() - 'yiiext'.
 	}
+
+	/* End all deprecated */
 }
