cdd6ae578c4407f2761e2406284928c00f5487c2
samdark <sam@rmcreative.ru>
2010-03-04 13:05:29 +0000

    imageColumn
    git-svn-id: http://yiiext.googlecode.com/svn/trunk@130 175f29da-e50b-11de-93f0-cd01e99e34ab

---
+++ app/extensions/yiiext/zii/widgets/grid/imageColumn/EImageColumn.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * EImageColumn
+ *
+ * Allows to display image in CGridView column.
+ *
+ * @author Alexander Makarov
+ *
+ * @version 1.0
+ *
+ */
+class EImageColumn extends CGridColumn {
+    /**
+	 * @var string a PHP expression that is evaluated for every data cell and whose result
+	 * is used as the CSS class name for the data cell. In this expression, the variable
+	 * <code>$row</code> the row number (zero-based); <code>$data</code> the data model for the row;
+	 * and <code>$this</code> the column object.
+	 */
+    public $imagePathExpression;
+
+    /**
+     * @var string Text that is used if cell is empty.
+     */
+    public $emptyText = '—';
+
+    /**
+     * @var int Image width.
+     */
+    public $width = null;
+
+    /**
+     * @var int Image height.
+     */
+    public $height = null;
+
+    /**
+	 * Renders the data cell content.	 
+	 * @param integer the row number (zero-based)
+	 * @param mixed the data associated with the row
+	 */
+	protected function renderDataCellContent($row,$data){
+        $content = $this->emptyText;
+        if($this->imagePathExpression!==null && $imagePath = $this->evaluateExpression($this->imagePathExpression,array('row'=>$row,'data'=>$data))){
+            $options = array();
+            $options['src'] = $imagePath;
+            if($this->width) $options['width'] = $this->width;
+            if($this->height) $options['height'] = $this->height;
+            $content = CHtml::tag('img', $options);			
+		}
+
+		echo $content;
+	}
+}
