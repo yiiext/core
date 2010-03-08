847f7a1ab88ebef6977c655d82f513a03e003e89
Veaceslav Medvedev <slavcopost@gmail.com>
2010-03-08 20:40:29 +0000

    git-svn-id: http://yiiext.googlecode.com/svn/trunk@154 175f29da-e50b-11de-93f0-cd01e99e34ab

--- app/extensions/yiiext/components/filesystem/EFile.php
+++ app/extensions/yiiext/components/filesystem/EFile.php
@@ -4,6 +4,7 @@
  *
  * @author Veaceslav Medvedev <slavcopost@gmail.com>
  * @version 0.1
+ * @package yiiext.filesystem
  */
 class EFile extends CComponent {
     private static $_files = array();   // file path => EFile
@@ -45,4 +46,93 @@ class EFile extends CComponent {
         return $this->_md;
     }
 
+    public function getParent() {
+        if ($this->_parent === NULL) {
+			$this->_parent = self::getInstance($this->getMetaData()->dirName);
+        }
+        return $this->_parent;
+    }
+
+    public function getFiles($filter = '*') {
+
+    }
+}
+
+/**
+ * EFileMetaData represents the meta-data for an EFile class.
+ *
+ * @author Veaceslav Medvedev <slavcopost@gmail.com>
+ * @version 0.1
+ * @package yiiext.filesystem
+ */
+
+class EFileMetaData {
+    protected $_attributes = array(
+        'name', 'path', 'dirName', 'type', 'extension', 'mime', 'isDir',
+        'size', 'permissions', 'modifiedTime', 'accessedTime',
+    );
+    protected $_file;
+    protected $_filePath;
+    protected $_metaData = array();
+
+    public function __construct($file) {
+        $this->_file = $file;
+        $this->_filePath = $file->getFilePath();
+        clearstatcache(TRUE, $this->_filePath);
+        foreach ($this->_attributes as $attribute) {
+            $this->__get($attribute);
+        }
+    }
+    public function __get($attribute) {
+        if (!in_array($attribute, $this->_attributes)) {
+            return NULL;
+        }
+        if (!isset($this->_metaData[$attribute])) {
+            $getter = 'get' . $attribute;
+		    if (method_exists($this, $getter)) {
+                $this->_metaData[$attribute] = $this->$getter();
+            }
+            else {
+                throw new CException(Yii::t('yiiext', 'Attribute {attribute} for file "{file}" not exists.',
+                    array('{attribute}' => $attribute, '{file}' => $this->_filePath)));
+            }
+        }
+        return $this->_metaData[$attribute];
+    }
+    public function toArray() {
+        return $this->_metaData;
+    }
+    public function getIsDir() {
+        return is_dir($this->_filePath);
+    }
+    public function getName() {
+        return basename($this->_filePath);
+    }
+    public function getPath() {
+        return $this->_filePath;
+    }
+    public function getDirName() {
+        return dirname($this->_filePath);
+    }
+    public function getType() {
+        return filetype($this->_filePath);
+    }
+    public function getExtension() {
+        return EFileHelper::fileExtension($this->_filePath);
+    }
+    public function getMime() {
+        return EFileHelper::getMimeType($this->_filePath);
+    }
+    public function getSize() {
+        return EFileHelper::fileSize($this->_filePath);
+    }
+    public function getPermissions() {
+        return fileperms($this->_filePath);
+    }
+    public function getModifiedTime() {
+        return filemtime($this->_filePath);
+    }
+    public function getAccessedTime() {
+        return fileatime($this->_filePath);
+    }
 }
