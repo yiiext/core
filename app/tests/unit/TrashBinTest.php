5983ea2e3979ece14b1d0c818dff3cd122416eed
Veaceslav Medvedev <slavcopost@gmail.com>
2010-09-03 18:41:48 +0000

    ETrashBinBehavior v.0.2
    git-svn-id: http://yiiext.googlecode.com/svn/trunk@263 175f29da-e50b-11de-93f0-cd01e99e34ab

--- app/tests/unit/TrashBinTest.php
+++ app/tests/unit/TrashBinTest.php
@@ -2,76 +2,73 @@
 /**
  * TrashBinTest
  */
+class TrashBinTest extends CDbTestCase
+{
+	public $fixtures=array(
+		'fruits'=>'Fruit',
+	);
 
-class TrashBinTest extends CDbTestCase {
-    public $fixtures=array(
-        'fruits'=>'Fruit',
-    );
+	function testRemove()
+	{
+		$this->setUp();
 
-    function testRemove(){
-        $this->setUp();
+		$fruit=Fruit::model()->findByPk(1);
+		$fruit->remove()->save();
 
-        $fruit = Fruit::model()->findByPk(1);
-        $fruit->remove()->save();
+		$fruits=Fruit::model()->findAll();
+		$this->assertEquals(2,count($fruits));
+	}
+	function testRestore()
+	{
+		$this->setUp();
 
-        $fruits = Fruit::model()->findAll();
-        $this->assertEquals(2, count($fruits));
-    }
+		$fruit=Fruit::model()->findByPk(1);
+		$fruit->remove()->save();
 
-     /**
-     * Will fail until
-     * http://code.google.com/p/yii/issues/detail?id=764
-     * is fixed
-     */
-    function testWithRemoved(){
-        $this->setUp();
+		$fruit=Fruit::model()->withRemoved()->findByPk(1);
+		$fruit->restore()->save();
 
-        $fruit = Fruit::model()->findByPk(1);
-        $fruit->remove()->save();
+		$fruits=Fruit::model()->findAll();
+		$this->assertEquals(3, count($fruits));
+	}
+	function testWithRemoved()
+	{
+		$this->setUp();
 
-        $fruits = Fruit::model()->findAll();
-        $this->assertEquals(2, count($fruits));
+		// Load all models excepts removed.
+		$fruits=Fruit::model()->findAll();
+		$this->assertEquals(3,count($fruits));
 
-        $fruits = Fruit::model()->withRemoved()->findAll();
-        $this->assertEquals(3, count($fruits));
+		// Remove model to trash.
+		$fruit=Fruit::model()->findByPk(1);
+		$fruit->remove()->save();
 
-        $fruits = Fruit::model()->findAll();
-        $this->assertEquals(2, count($fruits));
+		// Load all models include removed.
+		$fruits=Fruit::model()->withRemoved()->findAll();
+		$this->assertEquals(3,count($fruits));
 
-        $fruitsCount = Fruit::model()->filterRemoved()->count();
-        $this->assertEquals(2, $fruitsCount);
+		// Load all models excepts removed.
+		$fruits=Fruit::model()->findAll();
+		$this->assertEquals(2,count($fruits));
 
-        $fruitsCount = Fruit::model()->withRemoved()->count();
-        $this->assertEquals(3, $fruitsCount);
-    }
+		// Count models include removed.
+		$fruitsCount=Fruit::model()->withRemoved()->count();
+		$this->assertEquals(3,$fruitsCount);
 
-    /**
-     * @depends testWithRemoved
-     */
-    function testIsRemoved(){
-        $this->setUp();
+		// Count models excepts removed.
+		$fruitsCount=Fruit::model()->withRemoved()->filterRemoved()->count();
+		$this->assertEquals(2,$fruitsCount);
+	}
+	function testIsRemoved()
+	{
+		$this->setUp();
 
-        $fruit = Fruit::model()->findByPk(1);
-        $fruit->remove()->save();
+		// Remove model to trash.
+		$fruit=Fruit::model()->findByPk(1);
+		$fruit->remove()->save();
 
-        $fruit = Fruit::model()->withRemoved()->findByPk(1);        
-
-        $this->assertTrue($fruit->isRemoved());
-    }
-
-    /**
-     * @depends testWithRemoved
-     */
-    function testRestore(){
-        $this->setUp();
-
-        $fruit = Fruit::model()->findByPk(1);
-        $fruit->remove()->save();
-
-        $fruit = Fruit::model()->withRemoved()->findByPk(1);
-        $fruit->restore()->save();
-
-        $fruits = Fruit::model()->findAll();
-        $this->assertEquals(3, count($fruits));    
-    }
+		$fruit=Fruit::model()->withRemoved()->findByPk(1);
+		$this->assertTrue($fruit->getIsRemoved());
+		$this->assertTrue($fruit->isRemoved);
+	}
 }
