5bd24428262c7e9f43c9900984b92d13d0c9e5ca
samdark <sam@rmcreative.ru>
2010-09-25 00:07:09 +0000

    release command fix
    git-svn-id: http://yiiext.googlecode.com/svn/trunk@288 175f29da-e50b-11de-93f0-cd01e99e34ab

--- app/commands/Zip.php
+++ app/commands/Zip.php
@@ -3,14 +3,14 @@
  * Zip
  */
 class Zip extends ZipArchive {
-    public function addDir($path, $internalPath) {        
+    public function addDir($path, $internalPath) {
         $this->addEmptyDir($internalPath);
-        $nodes = glob($path.'/*');        
+        $nodes = glob($path.'/*');
 
         foreach ($nodes as $node) {
-            $addAs = $internalPath.'/'.basename($node);            
+            $addAs = $internalPath.'/'.basename($node);
             if (is_dir($node)) {
-                $this->addDir($node, $internalPath);
+                $this->addDir($node, $internalPath.'/'.basename($node));
             }
             if (is_file($node)) {
                 if(!$this->addFile($node, $addAs)) return false;
