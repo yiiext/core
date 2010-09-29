65f66e9b58ab1c7fc087f09617d300ce6ec548b7
samdark <sam@rmcreative.ru>
2010-09-29 15:08:21 +0000

    git-svn-id: http://yiiext.googlecode.com/svn/trunk@300 175f29da-e50b-11de-93f0-cd01e99e34ab

--- app/releases.bat
+++ app/releases.bat
@@ -1,13 +1,24 @@
-@rem php console.php release yiiext/components/shoppingCart 0.6
 @rem php console.php release yiiext/behaviors/model/status 0.6
 @rem php console.php release yiiext/behaviors/model/taggable 1.3
 @rem php console.php release yiiext/behaviors/model/ensureNull 1.0.1
-php console.php release yiiext/components/zendAutoloader 1.1
+@rem php console.php release yiiext/behaviors/model/trees 0.95
+@rem php console.php release yiiext/behaviors/model/eav 0.5
+@rem php console.php release yiiext/behaviors/model/trashBin 0.3
+
+@rem php console.php release yiiext/components/shoppingCart 0.7
+@rem php console.php release yiiext/components/zendAutoloader 1.1
+@rem php console.php release yiiext/components/improvedErrorHandler 1.1
+
 @rem php console.php release yiiext/filters/setReturnUrl 1.0.2
+
 @rem php console.php release yiiext/renderers/dwoo 0.9.1
 @rem php console.php release yiiext/renderers/quicky 0.9.1
 @rem php console.php release yiiext/renderers/smarty 0.9.7
 @rem php console.php release yiiext/renderers/twig 0.9.2
+
 @rem php console.php release yiiext/widgets/uplodify 1.3
+@rem php console.php release yiiext/widgets/markitup 1.0
+     php console.php release yiiext/widgets/iconizedMenu 1.1
 @rem php console.php release yiiext/zii/widgets/grid/imageColumn 1.1
-@rem php console.php release yiiext/behaviors/model/trees 0.90
+
+@rem php console.php release yiiext/modules/webshell 1.1
