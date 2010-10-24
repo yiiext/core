00cf0453e5a253635290ed1dd105de47d1491608
ekstazyst <ekstazyst@gmail.com>
2010-10-24 09:04:45 +0000

    git-svn-id: http://yiiext.googlecode.com/svn/trunk@317 175f29da-e50b-11de-93f0-cd01e99e34ab

---
+++ app/extensions/yiiext/widgets/elfinder/ElFinderInputWidget.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ElFinderInputWidget
+ *
+ * @author root
+ */
+class ElFinderInputWidget extends ElFinderWidget
+{
+    public function run()
+    {
+        
+    }
+    //put your code here
+}
