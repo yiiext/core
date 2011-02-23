8b51347718c4b259a4f13f3c5fe5eca2b285e2de
Veaceslav Medvedev <slavcopost@gmail.com>
2011-02-23 09:17:00 +0000

    [*] Change formId defaults to 'actionId-form', where 'actionId' is the action ID being filtered by this filter. [*] Change models scenario defaults to 'ajax-actionId'.
    git-svn-id: http://yiiext.googlecode.com/svn/trunk@379 175f29da-e50b-11de-93f0-cd01e99e34ab

--- app/extensions/yiiext/filters/activeFormValidation/EActiveFormValidationFilter.php
+++ app/extensions/yiiext/filters/activeFormValidation/EActiveFormValidationFilter.php
@@ -27,7 +27,7 @@
  * </pre>
  *
  * @author Veaceslav Medvedev <slavcopost@gmail.com>
- * @version 0.3
+ * @version 0.4
  * @package yiiext.filters.activeFormValidation
  */
 class EActiveFormValidationFilter extends CFilter
@@ -48,18 +48,12 @@ class EActiveFormValidationFilter extends CFilter
 	 * @var string the name of the parameter indicating the request is an AJAX request.
 	 * @see CActiveForm::$htmlOptions
 	 */
-	public $ajaxVar='ajax';
-
+	public $ajaxVar='ajax';	
 	/**
-	 * Initializes the filter.
-	 * @throws CException
-	 */
-	public function init()
-	{
-		if($this->formId===null)
-			throw new CException(Yii::t('yiiext','The "{property}" property cannot be empty.',array(
-				'{property}'=>'formId')));
-	}
+	* @var string the action ID being filtered by this filter.
+	*/
+	private $_actionId;
+
 	/**
 	 * Performs the pre-action filtering.
 	 * @param CFilterChain the filter chain that the filter is on.
@@ -68,12 +62,16 @@ class EActiveFormValidationFilter extends CFilter
 	 */
 	protected function preFilter($filterChain)
 	{
+		$this->_actionId=$filterChain->action->getId();
+		if($this->formId===null)
+		{
+			$this->formId=$this->_actionId.'-form';
+		}
 		if(isset($_POST[$this->ajaxVar])&&$_POST[$this->ajaxVar]===$this->formId)
 		{
 			$models=$this->getModelsInternal();
 			if(!empty($models))
 			{
-				//Yii::debug(Yii::ds($this->_models[0]->getScenario(),1,false));
 				echo CActiveForm::validate($models);
 				Yii::app()->end();
 			}
@@ -82,8 +80,14 @@ class EActiveFormValidationFilter extends CFilter
 	}
 	protected function createModel($modelClass,$scenario=NULL,$isNewRecord=true)
 	{
+		if($scenario===null)
+		{
+			$scenario='ajax-'.$this->_actionId;
+		}
 		if($isNewRecord)
+		{
 			return new $modelClass($scenario);
+		}
 		$model=CActiveRecord::model($modelClass);
 		$model->setScenario($scenario);
 		return $model;
@@ -96,15 +100,25 @@ class EActiveFormValidationFilter extends CFilter
 	{
 		$models=array();
 		if(!is_array($this->models))
+		{
 			$this->models=array($this->models);
+		}
 		foreach($this->models as $model)
 		{
 			if($model instanceof CModel)
+			{
 				$models[]=$model;
+			}
 			else if(is_string($model))
+			{
 				$models[]=$this->createModel($model);
+			}
 			else if(is_array($model))
+			{
+				isset($model['scenario'])||$model['scenario']=null;
+				isset($model['isNewRecord'])||$model['isNewRecord']=true;
 				$models[]=$this->createModel($model['modelClass'],$model['scenario'],$model['isNewRecord']);
+			}
 		}
 		return $models;
 	}
